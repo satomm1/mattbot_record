787371d3a57921a57516274dc7d364d5b167e74c
Matthew Sato <satomm@stanford.edu>
2024-10-30 00:02:51 +0000

    Improve analysis of sound

--- scripts/analyze_wav.py
+++ scripts/analyze_wav.py
@@ -35,20 +35,43 @@ print("Data shape: ", data.shape)
 print("Data type: ", data.dtype)
 # print("data: ", data)
 
-# Do fft 
-fft_result = np.fft.fft(data[:,0])
-print("fft_result: ", fft_result)
+# Do fft for left channel
+fft_result_left = np.fft.fft(data[:,0])
 
-# Plot the fft results showing only positive frequencies
-f = np.fft.fftfreq(len(fft_result), 1/samplerate)
+# Do fft for right channel
+fft_result_right = np.fft.fft(data[:,1])
+
+max_amplitude = np.max(np.abs(data))
+print("max_amplitude: ", max_amplitude)
+
+# Plot the fft results for both channels showing only positive frequencies in subplots
+f = np.fft.fftfreq(len(fft_result_left), 1/samplerate)
+
 plt.figure()
-plt.plot(f[:len(f)//2], np.abs(fft_result)[:len(f)//2])
+
+# Subplot for left channel
+plt.subplot(2, 1, 1)
+plt.title('FFT Results - Left Channel')
+plt.plot(f[:len(f)//2], np.abs(fft_result_left)[:len(f)//2])
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.grid()
+
+# Subplot for right channel
+plt.subplot(2, 1, 2)
+plt.title('FFT Results - Right Channel')
+plt.plot(f[:len(f)//2], np.abs(fft_result_right)[:len(f)//2])
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.grid()
+
+plt.tight_layout()
 plt.savefig('fft_result.png')
 
 # Plot the audio data
 plt.figure()
-plt.plot(data[:,0])
-plt.xlim(0, 10000)
+plt.plot(data[:,1][0:1000])
+plt.xlim(0, 1000)
 plt.savefig('audio_data.png')
 
 
